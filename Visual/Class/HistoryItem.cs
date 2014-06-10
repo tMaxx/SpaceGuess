c62e4699b03eec5a490ab87496920573c92573ce
Nicolas Waw <mikwaw@gmail.com>
2014-06-10 18:19:56 +0200

    Initial commit

---
+++ Visual/Class/HistoryItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prolog;
+
+namespace Visual.Class
+{
+    class HistoryItem
+    {
+        protected PrologEngine engine = null;
+
+        public HistoryItem()
+        {
+            this.engine = new PrologEngine()
+        }
+    }
+}
