626467630d770b3adb6614f901e99b7fa466d4ae
Nicolas Waw <mikwaw@gmail.com>
2014-06-21 13:55:43 +0200

    bump

--- MLAiP_ExplanationBased.pl
+++ MLAiP_ExplanationBased.pl
@@ -22,35 +22,3 @@ operational(part(_, _)).
 operational(owns(_, _)).
 operational(points_up(_)).
 operational(concave(_)).
-
-
-
-
-%functions
-extract_support(Proof, Proof) :- operational(Proof).
-extract_support((A :- _), A) :- operational(A).
-extract_support((AProof, BProof), (A, B)) :-
-	extract_support(AProof, A),
-	extract_support(BProof, B).
-extract_support((_ :- Proof), B) :-
-	extract_support(Proof, B).
-
-duplicate(Old, New) :-
-	assert('$marker'(Old)),
-	retract('$marker'(New)).
-
-prolog_ebg(A, GenA, A, GenA) :- clause(A, true).
-prolog_ebg((A, B), (GenA, GenB), (AProof, BProof), (GenAProof, GenBProof)) :- !,
-	prolog_ebg(A, GenA, AProof, GenAProof),
-	prolog_ebg(B, GenB, BProof, GenBProof).
-prolog_ebg(A, GenA, (A :- Proof), (GenA :- GenProof)) :-
-	clause(GenA, GenB),
-	duplicate((GenA :- GenB), (A :- B)),
-	prolog_ebg(B, GenB, Proof, GenProof).
-
-
-ebg(Goal, Gen_goal, (Gen_goal :- Premise)) :-
-	prolog_ebg(Goal, Gen_goal, _, Gen_proof),
-	extract_support(Gen_proof, Premise).
-
-
--- test.pl
+++
@@ -1,7 +0,0 @@
-%t(l, w, r).
-
-inBST(t(X, _, _), X) :- !.
-inBST(t(X, L, R), Elem) :- !,
-	R \= nil, Elem > X, inBST(R, Elem) ;
-	L \= nil, Elem < X, inBST(L, Elem).
-
